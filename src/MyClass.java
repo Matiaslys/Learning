9a2490a2126b0ca3174937e8ff8aca6e8915d2f6
Matiaslys <mati1857@edu.easj.dk>
2019-09-09 10:32:04 +0200

    updated

---
+++ src/MyClass.java
@@ -0,0 +1,10 @@
+class MyClass {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    System.out.print("the ");
+    System.out.print("world ");
+    System.out.print("is ");
+    System.out.print("big ");
+  }
+}
+
---
+++ src/Variabler.java
@@ -0,0 +1,35 @@
+public class Variabler {
+    public static void main(String[] args) {
+
+        //primitive datatyper
+        byte a = 127;
+        short c = 1001;
+        int b = 10024;
+
+        //decimaltal
+        double d = 2.7;
+        float e = 5.0f;
+        long storetal = 271238471;
+
+        //boolean
+        boolean f = true;
+        boolean g = false;
+
+        //string
+        String h = "heste";
+
+        //char
+        char i = 'b';
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+        System.out.println(a);
+    }
+}
